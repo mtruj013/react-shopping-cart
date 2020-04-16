8dc2ada7345171b6af9dc0fff9b0eacb5ba07cb2
Maria Trujillo <mptruj015@gmail.com>
2020-04-17 00:19:36 +0200

    added second context and wrapped all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts'
+import { CartContext } from './contexts'
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,16 +23,19 @@ function App() {
 		
 		<div className="App">
 			{console.log(cart)}
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<CartContext.Provider value={{cart}}>
+					<Navigation/>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products/>
+					</Route>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart/>
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
